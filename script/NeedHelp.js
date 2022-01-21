64eda85e73db0639d5ab4a2b23ee8018c9b8b15e
Niranjan <narayana9492324464@gmail.com>
2022-01-21 14:42:02 +0530

    NeedHelp page done

---
+++ script/NeedHelp.js
@@ -0,0 +1,9 @@
+function gotoLogin() {
+    window.location.href = 'login.html'
+
+}
+// function guide() {
+//     document.querySelector(".main_topics_div").hidden =true;
+
+// }
+// document.querySelector(".main_topics_div").hidden = false;
