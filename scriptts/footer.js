0884f73d0d68e6e687fea937c89b0d930867d7f9
Soubhagya118 <rinkisoubhagyasamal118@gmail.com>
2022-01-22 08:28:45 +0530

    addedftr

---
+++ scriptts/footer.js
@@ -0,0 +1,127 @@
+function footer(){
+    return` <div class= "topbar">
+       
+    <div class="tophead">
+    <h2 id="nav_heading">INDIA’S LARGEST HEALTHCARE PLATFORM</h2>
+    </div>
+    <div id="box">
+        <div>   
+    <div id = "box1">
+        160m+</div>
+    <div id="text">Visitors</div>
+</div>
+      <div>
+       <div id="box1">27m+</div>
+      <div id="text">Orders Delivered</div>
+  </div>
+
+<div>
+  <div id ="box1">1800+</div>
+     <div id="text">Cities</div>
+  </div> 
+</div>
+</div>
+</div>
+<div id="box5">
+<div id=box6>
+<p id="getapp">Get the link to download App</p>
+</div>
+<div id=box7>
+    <form>
+   <input type="tel" placeholder="Enter Phone Number" class = "text" maxlength="10" autocomplete="off" valuepattern="[6-9]\d{9}"required>
+    <button type="submit" id="butn">Send Link</button>   
+</form> 
+</div>
+</div> 
+
+</div>
+</div>
+
+<div id="container">
+
+<div id="firstbox">
+    <div><h3>Know Us</h3><p id="sub_info">About Us <br> Contact Us <br> Press Coverage <br> Careers <br> Business Partnership <br> Become a Health Partner</p> </div>
+    <div> <h3> Our Policies</h3>Privacy Policy <br>Terms and Conditions <br> Editorial Policy <br>Return Policy <br>IP Policy <br>Grievance Redressal Policy</div>
+    <div> <h3> Our Services</h3>Order Medicines <br>Book Lab <br>Consult a Doctor<br>Ayurveda Articles<br>Hindi Articles<br>Care Plan</div>
+    
+    <div id="connect" style="margin-right: 50px;">
+    <h3>Connect</h3>
+    <div>social link</div>
+    <div id="logo1">
+      
+        <div id="facbook"><a href="https://www.facebook.com/1mgOfficial"><img src="	https://www.pngarts.com/files/10/Facebook-Logo-Black-And-White-PNG-Background-Image.png" width="30" height="30px" alt="gvdrf">
+        </a></div>
+       
+        <div id="twitter"><a href="https://www.twitter.com/1mgOfficial"> <img src="	https://cdn-icons-png.flaticon.com/512/60/60580.png" width="35px"  height="35px"alt=""></a></div> 
+        
+        <div id="youtube"><a href="https://www.youtube.com/c/1mgofficial"><img src="	https://www.vectorico.com/download/social_media/youtube-dark-squircle.png" width="30px"  height="30px" alt=""> </a></div>
+        
+        <div id="linkedin"><a href="https://www.linkedin.com/company/1mg"><img src="	https://seeklogo.com/images/L/linkedin-black-icon-logo-ECC426C572-seeklogo.com.png"   width="30px"  height="30px" alt=""></a></div>
+     
+        <div id="medium"> <a href="https://medium.com/1mgofficial"><img src="	https://res.cloudinary.com/du8msdgbj/image/upload/v1601285810/marketing/jf3bry3wl6zzkd14yces.svg"  height="30px"  height="30px" alt=""></a></div>
+    </div>
+     <div>Want daily dose of health?</div>
+    <div><button>Sign Up</button></div>
+   </div>
+    
+     <div id="app"> <h3> Download App</h3><a href="https://play.google.com/store/apps/details?id=com.aranoah.healthkart.plus">
+        <div class="logo2"><img src="https://www.freepnglogos.com/uploads/app-store-logo-png/apple-app-store-travel-awards-globestamp-7.png" width="160px" height="100px"   alt=""></a></div>
+
+    </div>
+</div>
+<div class="line"></div>
+
+
+<div id="secondbox">
+<div class="box1"> 
+    <div class="smallbox" style=" background: url(https://res.cloudinary.com/du8msdgbj/image/upload/v1571132631/secure-rebrand_x6f8yq.svg) no-repeat;"></div>
+    <div class="bigbox"><h2> Reliable</h2>
+      <p style="color: #767676;font-size: 12px;line-height: 17px;"> All products displayed on Tata 1mg are procured from verified and licensed pharmacies. All labs listed on the platform are accredited </p> 
+    </div>
+</div>
+<div class="box1">
+    <div class="smallbox" style=" background: url(https://res.cloudinary.com/du8msdgbj/image/upload/v1571132500/reliable-rebrand_rcpof3.svg) no-repeat;"></div>
+    <div class="bigbox"><h2> Secure</h2>
+      <p style="color: #767676;font-size: 12px;line-height: 17px;"> All products displayed on Tata 1mg are procured from verified and licensed pharmacies. All labs listed on the platform are accredited </p> 
+    </div>
+</div>
+<div class="box1">
+    <div class="smallbox" style="background: url(https://res.cloudinary.com/du8msdgbj/image/upload/v1571131870/affordable-rebrand_ivgidq.svg) no-repeat;"></div>
+    <div class="bigbox"><h2> Affordable</h2>
+      <p style="color: #767676;font-size: 12px;line-height: 17px;"> All products displayed on Tata 1mg are procured from verified and licensed pharmacies. All labs listed on the platform are accredited </p> 
+    </div>
+</div>
+</div>
+<div class="line"></div>
+
+<div id="thirdbox">
+    <div class="logo3">
+        <div class="img1"><a href="http://legitscript.com" target="_blank" title="Verify LegitScript Approval"><img src="https://static.legitscript.com/seals/729605.png" alt="LegitScript approved" width="130" height="90"></a></div>
+        <div class="img2"><img src="	https://res.cloudinary.com/du8msdgbj/image/upload/v1580717209/URS_ebgmxc.png" width="110px" height="100px"alt=""></div>
+      
+    </div>
+    <div class="dec">
+        <h3 id="last_txt">India's only LegitScript and ISO/IEC 27001 certified online healthcare platform</h3>
+    </div>
+</div>
+
+
+<div class="line1" style="margin-top: 30px;"></div>
+<div id="forthbox">
+    
+<h3 >Know More About Tata 1mg</h3>
+<div><img src="	https://img.1mg.com/images/down-arrowRebrand.svg" wight="11px" height="9px"   alt=""></div>
+</div>
+<div class="line1"></div>
+</div>
+
+</div>
+<p id="reserve">© 2022 Tata 1mg. All rights reserved. In compliance with Drugs and Cosmetics Act, 1940 and Drugs and Cosmetics Rules, 1945, we don't process requests for Schedule X and other habit forming drugs.</p>`
+}
+export default footer
+
+
+
+
+
+
