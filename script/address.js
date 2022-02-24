3b3631388fd622b4f100d2f1e1b32480a923100d
rohan688 <gaikwadrohan2710@gmail.com>
2022-02-24 14:57:53 +0530

    Update address.js

--- script/address.js
+++ script/address.js
@@ -9,6 +9,32 @@ document.querySelector("#save_btn").addEventListener("click", (e) => {
     let customer_name=document.querySelector("#name").value;
     let number=document.querySelector("#mobile").value;
     
+    async function addresspost(){
+       
+    try{
+          let prd_body={
+  
+          building:building,
+          pin:pin,
+          locality:locality,
+          city:city,
+          state:state,
+          customer_name:customer_name,
+          number:number,
+         
+      }
+            let res = await fetch("https://tata1mg.herokuapp.com/products",{
+                method:"POST",
+                body:JSON.stringify(prd_body),
+                headers:{"Content-type":"application/json"}
+            });
+            let data = await res.json();
+            console.log(data);
+        }catch(err){
+            console.log(err);
+        }
+      }
+  
     
     
 
